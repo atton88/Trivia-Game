1ad4bcfa230e6e17353d6baaba038f461ccf3bb0
Atton <jabusau@gmail.com>
2018-08-09 23:51:27 -0700

    score page complete, need readme

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,10 +13,10 @@ $(document).ready(function() {
 
     //questions is an array of arrays with the [question, answer, array of answers]
     var questions = [
-        {q:"How many planets are in the solar system?", a:8, z:[8, 9, 10, 11]},
+        {q:"How many planets are in the solar system?", a:"8", z:["8", "9", "10", "11"]},
         {q:"What is the second biggest planet in our Solar System", a:"Saturn", z:["Jupiter", "Saturn", "Neptune", "Earth"]},
         {q:"What is the hottest planet in our Solar System?", a:"Venus", z:["Mercury", "Venus", "Earth", "Jupiter"]},
-        // ["How many moons have been discovered on Saturn?"],
+        // [],
         {q:"What is the funniest planet name?", a:"Uranus", z:["Jupiter", "Venus", "Uranus", "Kepler-22B"]},
         {q:"What is the closest star to Earth besides the sun?", a:"Alpha Centauri", z:["Sirius", "Epsilon", "Orion", "Alpha Centauri"]},
 
@@ -41,8 +41,7 @@ $(document).ready(function() {
         $("#timerDiv").append($("<h2>").text("Time remaining:"));
         $("#timerDiv").append($("<h1 id='timerText'>").text("_____"));
 
-
-
+        // clears interval and starts timer
         clearInterval(intervalId);
         intervalId = setInterval(decrement, 1000) // every second
     }
@@ -58,16 +57,19 @@ $(document).ready(function() {
         };
         if (count === 0) {
             $("#timerText").text(0);
-
             results();
         }
     }
 
     function generateQuestions() {
-        $("#questions").empty();
+        // hide start button during game
+        $("#startButton").css("display", "none")
 
+        // append questions
         for (var i = 0; i < questions.length; i++) {
             $("#questions").append("<hr>" + "<h3>" + questions[i].q + "</h>");
+
+            // create temp form
             var tempForm = $("<form>").attr("id", "form"+i);
 
             //create new loop to go through the answers array in Object question and create the radio buttons
@@ -83,26 +85,15 @@ $(document).ready(function() {
                 tempForm.append(tempInput);
                 tempForm.append(tempInput.attr("data-answer"));
             }
-
+            
             // append the tempForm to page
             $("#questions").append(tempForm);
         }
         $("#questions").append(tempForm);
         
         // create submit button
-
         createButton("Submit");
-        // var submitButton  = $("<button>").attr("id", "submitButton");
-        // submitButton.text("Submit");
-        // submitButton.attr("class", "btn btn-info btn-lg btn-block text-dark shadow font-weight-bold");
-    
-        // submitButton.css("margin-top", "30px");
-        // console.log(submitButton);
-        // $("#questions").append(submitButton);
-
     }
-
-    // startGame(); // initial start
 
     // On-click function, adds value to score, checks for win and loss conditions
     $("#startButton").on("click", startGame); 
@@ -112,9 +103,6 @@ $(document).ready(function() {
 
     // On-click function to restart the game after results page
     $(".container").on("click", "#restartButton", startGame);
-
-
-
 
     // Reset game function
     function startGame() {
@@ -128,24 +116,33 @@ $(document).ready(function() {
     // clears the screen and appends results
     function results() {
         clearInterval(intervalId);
+        
+        // Unhide start button
+        $("#startButton").css("display", "inline")
+
+        //check for correct and incorrect answers
+        for (var i = 0; i < questions.length; i++) {
+            console.log($("input[name=radio"+i+"]:checked").attr("data-answer"));
+            console.log(questions[i].a);
+            if (questions[i].a === $("input[name=radio"+i+"]:checked").attr("data-answer")) {
+                correct++;
+            }
+            else if ($("input[name=radio"+i+"]:checked").attr("data-answer")){
+                incorrect++;
+            }
+            console.log($("input[name=radio"+i+"]:checked"));
+                }
+
+        // clear appended elements
         $("#timerDiv").empty();
         $("#questions").empty();
-        for (var i = 0; i < questions.length; i++) {
-            // console.log($("input[name=radio1]:checked").val());
-            // console.log(document.getElementsByName("radio0"));
-            // if (questions[0].a === $("input[name=radio0]:checked").val()) {
-            // correct++;
-            // }
-            // else {
-            //     incorrect++;
-            // }
-        }
-        $("#timerDiv").append($("<h1>").text("Trivia Complete"));
+
+        // append results
+        $("#timerDiv").append($("<h3>").text("Trivia Complete"));
         $("#timerDiv").append($("<p>").text("Correct Answers: " + correct));
-        $("#timerDiv").append($("<p>").text("Correct Answers: " + incorrect));
+        $("#timerDiv").append($("<p>").text("Incorrect Answers: " + incorrect));
+        console.log(questions.length - (correct + incorrect));
         $("#timerDiv").append($("<p>").text("Unanswered: " + (questions.length - (correct + incorrect))));
-
-
     }
 
     // Creates submit button and restart button at the bottom of the page
